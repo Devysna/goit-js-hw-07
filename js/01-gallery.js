d5ed94c6ca441d215a0fc40f94c79e25d159cf67
Devysna <yashturman@gmail.com>
2023-02-27 04:28:00 +0200

    Рішення до завдань

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,51 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+const newGalleryItems = [];
+
+galleryItems.forEach(galleryElement => {
+
+    const galleryItem = document.createElement('div');
+        galleryItem.className = 'gallery__item';
+
+    const galleryLink = document.createElement('a');
+        galleryLink.className = 'gallery__link';
+        galleryLink.href = galleryElement.original;
+
+    const galleryImage = document.createElement('img');
+        galleryImage.className = 'gallery__image';
+        galleryImage.src = galleryElement.preview;
+        galleryImage.setAttribute('data-source', galleryElement.original);
+        galleryImage.alt = galleryElement.description;
+
+    galleryItem.append(galleryLink);
+    galleryLink.append(galleryImage);
+    newGalleryItems.push(galleryItem);
+});
+
+gallery.append(...newGalleryItems);
+
+gallery.addEventListener('click', clickSelectedImage);
+
+function clickSelectedImage (event) {
+    event.preventDefault();
+    if (event.target.nodeName !== 'IMG') {
+		return;
+	};
+
+    const instance = basicLightbox.create(`
+    <img src="${event.target.getAttribute('data-source')}" width="800" height="600">
+   `)
+
+    instance.show();
+
+    gallery.addEventListener('keydown', closeGalleryEscape, { once: true });
+
+    function closeGalleryEscape (event) {
+        if (event.key === 'Escape') {
+            instance.close()
+        }
+    };  
+};
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,32 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+const newGalleryItems = [];
+
+galleryItems.forEach(galleryElement => {    
+
+    const galleryItem = document.createElement('li');
+        galleryItem.className = 'gallery__item';
+
+	const galleryLink = document.createElement('a');
+	    galleryLink.className = 'gallery__link';    
+	    galleryLink.href = galleryElement.original;
+
+	const galleryImage = document.createElement('img');
+	    galleryImage.className = 'gallery__image';
+	    galleryImage.src = galleryElement.preview;
+	    galleryImage.setAttribute('title', galleryElement.description);
+	    galleryImage.alt = galleryElement.description;
+
+    galleryItem.append(galleryLink);
+    galleryLink.append(galleryImage);
+	newGalleryItems.push(galleryItem);
+});
+
+gallery.append(...newGalleryItems);
+
+new SimpleLightbox('.gallery a', {
+	captionDelay: 250
+});
